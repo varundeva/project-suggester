d19be04a856efa1bbba806988e040bb56905e98a
Varun Deva <varundev23@gmail.com>
2025-01-15 23:26:56 +0530

    fix: session

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,16 +7,14 @@ import { SessionProvider } from "next-auth/react";
 
 export default function RootLayout({
   children,
-  session
 }: Readonly<{
   children: React.ReactNode;
-  session: any
 }>) {
   return (
     <html lang="en">
       <body className="min-h-screen">
         <Navbar />
-        <SessionProvider session={session}>
+        <SessionProvider>
 
       {children}
         </SessionProvider>
